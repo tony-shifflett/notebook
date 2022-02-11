42affec1196cc9a77a66109af481b7a57148fda4
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-02-10 19:12:29 -0600

    completed trivial example of returning jsx from a map function in component

--- assessments/woven/tech-test/src/components/RenderedCard.js
+++ assessments/woven/tech-test/src/components/RenderedCard.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Data from '../json/Data.json'
 
 const RenderedCard = () => {
-  return (
-    <div>RenderedCard</div>
-  )
+ let card =
+    <div>
+        {/* The contents of this div are trivial. The purpose is to experiment with returning elements from the map function  */}
+        These are the data points:
+        {Data.map((datum => <p>{datum.point}-{datum.point}</p>))}
+        The above constitues a full list of the data points.
+    </div>
+  return card
 }
 
 export default RenderedCard
